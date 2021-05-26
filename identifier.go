dad097a8308345ae2b1c37b5dcc17b0c2be3df80
Thomas Schenker <tommzn@gmx.de>
2021-05-26 19:53:39 +0200

    update docs

--- identifier.go
+++ identifier.go
@@ -1,3 +1,4 @@
+// Package utils provides several helper function for golang.
 package utils
 
 import (
@@ -6,10 +7,12 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
+// NewId returns a new UUID version 4.
 func NewId() string {
 	return uuid.NewV4().String()
 }
 
+// IsId returns true if passed id is a UUID version 4.
 func IsId(id string) bool {
 	r := regexp.MustCompile("^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$")
 	return r.MatchString(id)
--- identifier_test.go
+++ identifier_test.go
@@ -7,14 +7,17 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
+// IdentifierTestSuite runs all test for id relaated functions.
 type IdentifierTestSuite struct {
 	suite.Suite
 }
 
+// TestIdentifierTestSuite executes all tests from IdentifierTestSuite.
 func TestIdentifierTestSuite(t *testing.T) {
 	suite.Run(t, new(IdentifierTestSuite))
 }
 
+// TestGenrateIds test generating new ids.
 func (suite *IdentifierTestSuite) TestGenrateIds() {
 
 	id1 := NewId()
@@ -23,6 +26,7 @@ func (suite *IdentifierTestSuite) TestGenrateIds() {
 	suite.Nil(err)
 }
 
+// Test asserting ids in UUID version 4.
 func (suite *IdentifierTestSuite) TestValidaeIds() {
 
 	suite.True(IsId(uuid.NewV4().String()))
